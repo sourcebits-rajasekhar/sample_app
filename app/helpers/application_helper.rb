4c5b917fe516476938857e03f10841b1774c5c74
rajasekhar <rajasekhar.reddy@sourcebits.com>
2013-01-23 12:43:08 +0530

    make a basic user model with secure password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,20 @@
 module ApplicationHelper
 
+
+
 	def full_title(page_title)
-    base_title = "Ruby"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
+
+	    base_title = "Ruby"
+	    
+	    if page_title.empty?
+	      base_title
+	    else
+	      "#{base_title} | #{page_title}"
+	    end
+	    
+
+	end
+
+
+
 end
