73ef4becace5363acc54cf531bd2c2cbf06278f0
rajasekhar <rajasekhar.reddy@sourcebits.com>
2013-01-23 19:38:56 +0530

    sign up page

--- Gemfile
+++ Gemfile
@@ -45,7 +45,10 @@ end
 # To use debugger
 # gem 'debugger'
 group :test do
+  
 	gem 'capybara','1.1.2'
+  gem 'factory_girl_rails','4.1.0'
+
 end
 group :production do 
 	gem 'pg','0.12.2'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,7 @@ describe User do
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
     it { should respond_to(:authenticate) }
-    
+
     it {should be_valid }
 
    describe "when name is not present" do
@@ -87,5 +87,26 @@ describe "when passwordconfirmation is nil" do
 
 end
   # pending "add some examples to (or delete) #{__FILE__}"
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
+end
   
 end
